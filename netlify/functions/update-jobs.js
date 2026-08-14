eb8b5358fdc1ac2ec471e78b8a09aeae9314ef5b
kiinhomlk-sudo <kiinhomlk@gmail.com>
2026-08-14 18:57:14 -0300

    Update update-jobs.js

--- netlify/functions/update-jobs.js
+++ netlify/functions/update-jobs.js
@@ -3,15 +3,15 @@ const { getStore } = require("@netlify/blobs");
 const FEEDS = [
   {
     name: "H-2A Job Orders",
-    url: "https://seasonaljobs.dol.gov/api/feeds/790"
+    url: "https://seasonaljobs.dol.gov/feeds/790.json"
   },
   {
     name: "H-2A Applications",
-    url: "https://seasonaljobs.dol.gov/api/feeds/9142A"
+    url: "https://seasonaljobs.dol.gov/feeds/9142A.json"
   },
   {
     name: "H-2B Applications",
-    url: "https://seasonaljobs.dol.gov/api/feeds/9142B"
+    url: "https://seasonaljobs.dol.gov/feeds/9142B.json"
   }
 ];
 
